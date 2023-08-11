c3f9def5f77e064eb9837126909a6798da03870f
Fernando <48518539+mobilepadawan@users.noreply.github.com>
2023-08-11 18:30:06 -0300

    Reposting again the Streamberry Repository on Github

---
+++ js/main.js
@@ -0,0 +1,169 @@
+import { apiKey, language, URI, URISeriesCat, URIMoviesCat } from '../JS/tmdbapi.js'
+const catalog = []
+const categories = []
+const container = document.querySelector('.container')
+const containerCat = document.querySelector('.genres')
+const pages = document.querySelectorAll('div.genres span')
+const searchButton = document.querySelector('#searchButton')
+const inputSearch = document.querySelector('input[type=search]')
+
+searchButton.addEventListener('click', ()=> {
+    document.querySelector('div.search-box').classList.add('search-box-active')
+    setTimeout(()=> inputSearch.focus(), 1000)
+})
+
+inputSearch.addEventListener('blur', ()=> {
+    document.querySelector('div.search-box').classList.remove('search-box-active')
+})
+
+const getCategories = async ()=> {
+    const APIURL = `${URIMoviesCat}`
+    fetch(APIURL)
+    .then(response => response.json())
+    .then(data => categories.push(...data.genres))
+    .then(()=> loadCategories(catalog))
+}
+
+const loadCategories = ()=> {
+    let contentToShow = ''
+    if (categories.length > 0) {
+        categories.forEach(cate => contentToShow += `<span id="${cate.id}">${cate.name}</span>`)
+        containerCat.innerHTML = contentToShow
+    }
+}
+
+const getMovies = async (page)=> {
+    const APIURL = `${URI}?api_key=${apiKey}&language=${language}&page=${page}`
+    fetch(APIURL)
+    .then(response => response.json())
+    .then(data => {
+        catalog.length = 0
+        catalog.push(...data.results)
+    })
+    .then(()=> loadMovies(catalog))
+    .then(()=> setClickInCovers())
+}
+getMovies(1)
+
+const setClickInCovers = ()=> {    
+    const allCovers = document.querySelectorAll('img.card-movie-img')
+   if (allCovers.length > 0) {
+        allCovers.forEach(cover => {
+            cover.addEventListener('click', ()=> {
+                showMovieDetails(cover.id)
+            })
+        })
+    }
+}
+
+const getActors = async (movieId)=> {
+    const casting = []
+    let reparto = ''
+    const APIURL = `https://api.themoviedb.org/3/movie/${movieId}/credits?api_key=${apiKey}`
+    return fetch(APIURL)
+    .then(response => response.json())
+    .then(data => {
+        casting.push(...data.cast)
+        for (let actor of casting)
+            reparto += `<p>${actor.name} como ${actor.character}</p>`
+    })
+    .then(()=> reparto)
+}
+
+const getVideos = async (movieId)=> {
+    let videoSelected = ''
+    const APIURL = `https://api.themoviedb.org/3/movie/${movieId}/videos?api_key=${apiKey}`
+    return fetch(APIURL)
+    .then(response => response.json())
+    .then(data => {
+        console.table(data.results)
+        const videoSelected = data.results.find(video => video.name.includes('Official' || 'Oficial'))
+        if (videoSelected) {
+            return videoSelected
+        }
+    })
+    .then(()=> `https://www.youtube.com/watch/${videoSelected}`)
+}
+
+const returnStars = (number)=> {
+    let starsToReturn = ''
+    for (let i = 1; i <= parseInt(number); i++) {
+        starsToReturn += '⭐️'
+    }
+    return starsToReturn
+}
+
+const showMovieDetails = async (movieId)=> {
+    // getVideos(movieId)
+    const movieSelected = catalog.find((catalog)=> catalog.id === parseInt(movieId))
+    if (movieSelected) {
+        document.querySelector('img#movieBackdrop').src = `https://image.tmdb.org/t/p/w780${movieSelected.backdrop_path}`
+        document.querySelector('h2#titleDetail').textContent = movieSelected.title
+        document.querySelector('h2.close-btn').addEventListener('click', ()=> {
+            movieDialog.close()
+            document.querySelector('img#movieBackdrop').src = ""
+            document.querySelector('h2#titleDetail').textContent = ""
+            document.querySelector('p#sinopsisDetail').textContent = `N / A`
+            document.querySelector('span#showStars').textContent = ""
+            document.querySelector('p#reparto').textContent = ''
+            document.body.style.overflow = ''
+        })
+        document.querySelector('p#sinopsisDetail').textContent = movieSelected.overview || `N / A`
+        document.querySelector('span#showStars').textContent = returnStars(movieSelected.vote_average)
+        document.querySelector('span#showStars').title = `${movieSelected.vote_average} / 10`
+        document.querySelector('p#reparto').innerHTML =  await getActors(movieId)
+        const movieDialog = document.querySelector('dialog')
+        setTimeout(() => {
+            movieDialog.showModal()
+            movieDialog.scrollTo({ top: 0})
+            document.body.style.overflow = 'hidden'
+            const detalleTitulo = document.querySelector('h2#titleDetail')
+            const catalogoCerrar = document.querySelector('h2.close-btn')
+            const imagenCatalogo = document.querySelector('img#movieBackdrop')
+            movieDialog.addEventListener('scroll', () => {
+                if (detalleTitulo.clientHeight >= 90 && (detalleTitulo.scrollTop - movieDialog.scrollTop <= -140)) {
+                    detalleTitulo.classList.add('dialog-hidden')
+                    catalogoCerrar.classList.add('dialog-hidden')
+            } else if (detalleTitulo.clientHeight <= 90 && (detalleTitulo.scrollTop - movieDialog.scrollTop <= -200)){
+                    detalleTitulo.classList.add('dialog-hidden')
+                    catalogoCerrar.classList.add('dialog-hidden')
+                } else {
+                    detalleTitulo.classList.remove('dialog-hidden')
+                    catalogoCerrar.classList.remove('dialog-hidden')
+                }
+            })
+        }, 200)
+    }
+}
+
+if (pages.length > 0) {
+    pages.forEach(page => {
+        page.addEventListener('click', ()=> getMovies(page.textContent))
+    })
+}
+
+const loadMovies = (catalog)=> {
+    let contentToShow = ''
+        if (catalog.length > 0) {
+            catalog.forEach(content => contentToShow += returnContentCard(content) )
+            container.innerHTML = contentToShow
+        }
+}
+
+const loadMainCover = ()=> {
+    return `<div class="header">
+                <h1 class="logo">STREAMBERRYFLIX</h1>
+            </div>`
+}
+
+const returnContentCard = (content)=> {
+    const posterURL = `https://image.tmdb.org/t/p/w185/${content.poster_path}`
+    return `<div class="card-movie">
+                <div>
+                    <img loading="lazy" class="card-movie-img" src="${posterURL}" title="${content.title}" id="${content.id}">
+                </div>
+                <div class="card-movie-title">
+                    <p>${content.title}</p>
+                </div>
+            </div>`
+}
---
+++ js/tmdbapi.js
@@ -0,0 +1,7 @@
+const apiKey = '01eab07be70818e7485f3e66560bb1a2'
+const URI = 'https://api.themoviedb.org/3/movie/popular'
+const URIMoviesCat = 'https://api.themoviedb.org/3/genre/movie/list?api_key=01eab07be70818e7485f3e66560bb1a2&language=es-MX'
+const URISeriesCat = 'https://api.themoviedb.org/3/genre/tv/list?api_key=01eab07be70818e7485f3e66560bb1a2&language=es-MX'
+const language = 'es-MX'
+
+export { apiKey, URI, language, URIMoviesCat, URISeriesCat } 
