bcab8f7959c74f3047c6c4e6719713d5f0529b7a
Fernando <48518539+mobilepadawan@users.noreply.github.com>
2023-08-16 20:24:17 -0300

    Rebranding all the esthetics for Streamberryflix

---
+++ js/apiconnect.js
@@ -0,0 +1,8 @@
+const apiKey = '01eab07be70818e7485f3e66560bb1a2'
+const BASEURI = 'https://api.themoviedb.org/3/movie/popular'
+const URIMoviesCat = `https://api.themoviedb.org/3/movie/popular?api_key=01eab07be70818e7485f3e66560bb1a2&language=es-MX`
+// const URISeriesCat = `https://api.themoviedb.org/3/tv/popular?api_key=01eab07be70818e7485f3e66560bb1a2&language=es-MX`
+const URISeriesCat = `https://api.themoviedb.org/3/genre/tv/list?api_key=01eab07be70818e7485f3e66560bb1a2&language=es-MX`
+const language = 'es-MX'
+
+export { apiKey, language, URIMoviesCat, URISeriesCat } 
--- js/main.js
+++ js/main.js
@@ -1,44 +1,51 @@
-import { apiKey, language, URI, URISeriesCat, URIMoviesCat } from '../js/tmdbapi.js'
+import { apiKey, language, URISeriesCat, URIMoviesCat } from './apiconnect.js'
 const catalog = []
-const categories = []
 const promotingContent = []
 const container = document.querySelector('.container')
-const containerCat = document.querySelector('.genres')
-const pages = document.querySelectorAll('div.genres span')
-const searchButton = document.querySelector('#searchButton')
-const inputSearch = document.querySelector('input[type=search]')
+const searchBox = document.querySelector('.search-box')
+let counter = 1
 let index = 0
 
+// SEARCH BOX CONTROL
+const searcBox = document.querySelector('div.search-box')
+const inputSearch = document.querySelector('input#inputSearch')
+
 searchButton.addEventListener('click', ()=> {
-    document.querySelector('div.search-box').classList.add('search-box-active')
-    setTimeout(()=> inputSearch.focus(), 1000)
+    inputSearch.value = ''
+    inputSearch.focus()
+    setTimeout(() => searchBox.classList.replace('search-box-closed', 'search-box-active'), 200)
+    window.innerWidth <= 580 && document.querySelector('.header-title').classList.add('hide')
 })
 
 inputSearch.addEventListener('blur', ()=> {
-    document.querySelector('div.search-box').classList.remove('search-box-active')
+    searcBox.classList.replace('search-box-active', 'search-box-closed')
+    setTimeout(() => window.innerWidth <= 580 && document.querySelector('.header-title').classList.remove('hide'), 500);
 })
 
-const getCategories = async ()=> {
-    const APIURL = `${URIMoviesCat}`
+// MOVIES AND SERIES CATALOG
+const getMovies = async (URI, page)=> {
+    const APIURL = `${URI}&page=${page}`
     fetch(APIURL)
     .then(response => response.json())
-    .then(data => categories.push(...data.genres))
-    .then(()=> loadCategories(catalog))
+    .then(data => {
+        catalog.length = 0
+        catalog.push(...data.results)
+    })
+    .then(()=> buildContentPromoted())
+    .then(()=> showContentPromoted())
+    .then(()=> loadMovies(catalog))
+    .then(()=> counter++)
+    .then(()=> {const divMore = document.querySelector('div#divMore')})
+    .then(()=> divMore.addEventListener('click', ()=> {
+        getMovies(URIMoviesCat, counter)
+    }))
 }
-
-const loadCategories = ()=> {
-    let contentToShow = ''
-    if (categories.length > 0) {
-        categories.forEach(cate => contentToShow += `<span id="${cate.id}">${cate.name}</span>`)
-        containerCat.innerHTML = contentToShow
-    }
-}
+getMovies(URIMoviesCat, counter)
+console.log('Valor de counter: ', counter)
 
 const buildContentPromoted = ()=> {
     for (let i = 0; i < 6; i++) {
         const randomNumber = Math.floor(Math.random() * 20)
-        console.log(randomNumber)
-        console.log(`https://image.tmdb.org/t/p/w1280${catalog[randomNumber].backdrop_path}`)
         const ContentToAdd = {
             backdrop: `https://image.tmdb.org/t/p/w1280${catalog[randomNumber].backdrop_path}`,
             title: catalog[randomNumber].title,
@@ -49,144 +56,20 @@ const buildContentPromoted = ()=> {
     console.table(promotingContent)
 }
 
-setInterval(() => {
-    showContentPromoted()
-}, 20000);
-
-const showContentPromoted = ()=> {
-    if (index === 5) {
-        index = 0
-    } else {
-        ++index
-    }
-    document.querySelector('div.promoting img').src = promotingContent[index].backdrop
-    document.querySelector('div.promoting h3').textContent = promotingContent[index].title
-    document.querySelector('div.promoting p').textContent = promotingContent[index].overview
-}
-
-const getMovies = async (page)=> {
-    const APIURL = `${URI}?api_key=${apiKey}&language=${language}&page=${page}`
-    fetch(APIURL)
-    .then(response => response.json())
-    .then(data => {
-        catalog.length = 0
-        catalog.push(...data.results)
-    })
-    .then(()=> loadMovies(catalog))
-    .then(()=> setClickInCovers())
-    .then(()=> buildContentPromoted())
-    .then(()=> showContentPromoted())
-}
-getMovies(1)
-
-const setClickInCovers = ()=> {    
-    const allCovers = document.querySelectorAll('img.card-movie-img')
-   if (allCovers.length > 0) {
-        allCovers.forEach(cover => {
-            cover.addEventListener('click', ()=> {
-                showMovieDetails(cover.id)
-            })
-        })
-    }
-}
-
-const getActors = async (movieId)=> {
-    const casting = []
-    let reparto = ''
-    const APIURL = `https://api.themoviedb.org/3/movie/${movieId}/credits?api_key=${apiKey}`
-    return fetch(APIURL)
-    .then(response => response.json())
-    .then(data => {
-        casting.push(...data.cast)
-        for (let actor of casting)
-            reparto += `<p>${actor.name} como ${actor.character}</p>`
-    })
-    .then(()=> reparto)
-}
-
-const getVideos = async (movieId)=> {
-    let videoSelected = ''
-    const APIURL = `https://api.themoviedb.org/3/movie/${movieId}/videos?api_key=${apiKey}`
-    return fetch(APIURL)
-    .then(response => response.json())
-    .then(data => {
-        console.table(data.results)
-        const videoSelected = data.results.find(video => video.name.includes('Official' || 'Oficial'))
-        if (videoSelected) {
-            return videoSelected
-        }
-    })
-    .then(()=> `https://www.youtube.com/watch/${videoSelected}`)
-}
-
-const returnStars = (number)=> {
-    let starsToReturn = ''
-    for (let i = 1; i <= parseInt(number); i++) {
-        starsToReturn += '⭐️'
-    }
-    return starsToReturn
-}
-
-const showMovieDetails = async (movieId)=> {
-    // getVideos(movieId)
-    const movieSelected = catalog.find((catalog)=> catalog.id === parseInt(movieId))
-    if (movieSelected) {
-        document.querySelector('img#movieBackdrop').src = `https://image.tmdb.org/t/p/w1280${movieSelected.backdrop_path}`
-        document.querySelector('h2#titleDetail').textContent = movieSelected.title
-        document.querySelector('h2.close-btn').addEventListener('click', ()=> {
-            movieDialog.close()
-            document.querySelector('img#movieBackdrop').src = ""
-            document.querySelector('h2#titleDetail').textContent = ""
-            document.querySelector('p#sinopsisDetail').textContent = `N / A`
-            document.querySelector('span#showStars').textContent = ""
-            document.querySelector('p#reparto').textContent = ''
-            document.body.style.overflow = ''
-        })
-        document.querySelector('p#sinopsisDetail').textContent = movieSelected.overview || `N / A`
-        document.querySelector('span#showStars').textContent = returnStars(movieSelected.vote_average)
-        document.querySelector('span#showStars').title = `${movieSelected.vote_average} / 10`
-        document.querySelector('p#reparto').innerHTML =  await getActors(movieId)
-        const movieDialog = document.querySelector('dialog')
-        setTimeout(() => {
-            movieDialog.showModal()
-            movieDialog.scrollTo({ top: 0})
-            document.body.style.overflow = 'hidden'
-            const detalleTitulo = document.querySelector('h2#titleDetail')
-            const catalogoCerrar = document.querySelector('h2.close-btn')
-            const imagenCatalogo = document.querySelector('img#movieBackdrop')
-            movieDialog.addEventListener('scroll', () => {
-                if (detalleTitulo.clientHeight >= 90 && (detalleTitulo.scrollTop - movieDialog.scrollTop <= -140)) {
-                    detalleTitulo.classList.add('dialog-hidden')
-                    catalogoCerrar.classList.add('dialog-hidden')
-            } else if (detalleTitulo.clientHeight <= 90 && (detalleTitulo.scrollTop - movieDialog.scrollTop <= -200)){
-                    detalleTitulo.classList.add('dialog-hidden')
-                    catalogoCerrar.classList.add('dialog-hidden')
-                } else {
-                    detalleTitulo.classList.remove('dialog-hidden')
-                    catalogoCerrar.classList.remove('dialog-hidden')
-                }
-            })
-        }, 200)
-    }
-}
-
-if (pages.length > 0) {
-    pages.forEach(page => {
-        page.addEventListener('click', ()=> getMovies(page.textContent))
-    })
-}
-
 const loadMovies = (catalog)=> {
+    document.querySelector('.loader-animation') && document.querySelector('.loader-animation').remove()
+    document.querySelector('div#divMore')  &&  document.querySelector('div#divMore').remove()
     let contentToShow = ''
         if (catalog.length > 0) {
             catalog.forEach(content => contentToShow += returnContentCard(content) )
-            container.innerHTML = contentToShow
+            container.innerHTML += contentToShow
+            container.innerHTML += returnMoreContentCard()
         }
 }
 
-const loadMainCover = ()=> {
-    return `<div class="header">
-                <h1 class="logo">STREAMBERRYFLIX</h1>
+const returnMoreContentCard = ()=> {
+    return `<div class="card-content" title="Cargar más..." id="divMore">
+                <h3>+</h3>
             </div>`
 }
 
@@ -194,10 +77,27 @@ const returnContentCard = (content)=> {
     const posterURL = `https://image.tmdb.org/t/p/w185/${content.poster_path}`
     return `<div class="card-movie">
                 <div>
-                    <img loading="lazy" class="card-movie-img" src="${posterURL}" title="${content.title}" id="${content.id}">
+                    <img loading="lazy" class="card-movie-img" src="${posterURL}" 
+                         title="${content.title}" id="${content.id}" alt="${content.original_title}" >
                 </div>
                 <div class="card-movie-title">
                     <p>${content.title}</p>
                 </div>
             </div>`
 }
+
+const showContentPromoted = ()=> {
+    index === 5 ? index = 0 : ++index
+    document.querySelector('content-promoted, img').src = promotingContent[index].backdrop
+    document.querySelector('content-promoted, img').alt = promotingContent[index].title
+    document.querySelector('content-promoted, h2').textContent = promotingContent[index].title
+    if (window.innerWidth <= '360') {
+        document.querySelector('content-promoted, p').textContent = promotingContent[index].overview.toString().slice(0, 200).concat("...")
+    } else {
+        document.querySelector('content-promoted, p').textContent = promotingContent[index].overview.toString()
+    }
+}
+
+setInterval(() => {
+    showContentPromoted()
+}, 20000)
